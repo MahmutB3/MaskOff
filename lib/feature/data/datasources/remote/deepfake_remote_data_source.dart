89280bac8dbe548793bbece7ac12825b95ca1c1d
Mahmut Basmacı <148640537+MahmutB3@users.noreply.github.com>
2026-04-16 21:26:24 +0300

    implement deepfake remote data source and configure Android build environment

--- lib/feature/data/datasources/remote/deepfake_remote_data_source.dart
+++ lib/feature/data/datasources/remote/deepfake_remote_data_source.dart
@@ -11,7 +11,7 @@ abstract class DeepfakeRemoteDataSource {
 
 class DeepfakeRemoteDataSourceImpl implements DeepfakeRemoteDataSource {
   //final String baseUrl = 'http://10.0.2.2:5000';
-  final String baseUrl = 'https://89b1-85-105-61-128.ngrok-free.app';
+  final String baseUrl = 'https://f80f-213-153-145-22.ngrok-free.app';
 
   @override
   Future<DeepfakeResultModel> analyzeVideo(File videoFile) async {
